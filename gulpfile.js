4b9e50f7b9c9e336c8f87e9d98ebeafcc352c087
Brenno Ogeda <brennoogeda@gmail.com>
2017-04-04 00:24:44 -0300

    broserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,33 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', compileLess);
+// gulp.task('default', ['less','sync', 'watch']);
+gulp.task('less', compileLess);
+gulp.task('watch', watch);
+gulp.task('sync', browserSync);
+gulp.task('default', ['less'], function() {
+	
+});
 
 function compileLess() {
 	gulp
-		.src('./less/**/*.less')
+		.src('./public/less/**/*.less')
 	    .pipe(less())
 	    .pipe(gulp.dest('./public/css/'));
 };
+
+function watch() {
+  	gulp.watch("./public/less/**/*.less", ['less']);
+    gulp.watch("./public/*.html").on('change', browserSync.reload);
+    gulp.watch("./public/css/*.css").on('change', browserSync.reload);
+}
+
+function browserSync() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+}
